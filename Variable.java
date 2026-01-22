f5fb2771e78b3eca41d3071a34197b172b3950da
Rajshri111-max <rkalshetti111@gmail.com>
2026-01-21 20:00:04 -0800

    Add files via upload
    Variable and how to use differant type of variables

---
+++ Variable.java
@@ -0,0 +1,28 @@
+public class Variable {
+    public static void main(String[] args)
+     { 
+       // int num = 3;
+       // int num1 = 10;
+       // int result = num +num1;
+       // System.out.println(result);
+
+       //literals
+       int num1 = 0b101;
+       System.out.println(num1);
+      int num2 = 0x7E;
+       System.out.println(num2);
+      // integer gets stored in float value if u assign int value to floate type
+      double num = 56;
+      System.out.println(num);
+      
+      
+      char c= 'a';
+      c++;
+      System.out.println(c);
+
+
+
+        
+       }
+    }
+
